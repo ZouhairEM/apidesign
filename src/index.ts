54d09f1fc7fa994ce8e4a694b011193ab5f1117c
ZouhairEM <zouhairelmariami@gmail.com>
2024-01-09 09:09:54 +0100

    add auth with jwt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import path from 'path';
 import router from './router';
 import morgan from 'morgan';
+import * as dotenv from 'dotenv' 
+import { protect } from './modules/auth';
+dotenv.config();
 
 const app = express();
 
@@ -9,14 +11,15 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use((req, res, next) => {
+app.use('/api',  protect, router)
+app.use((req, _, next) => {
   req.shhh_secret = 'doggy'
   next()
 })
 
-app.use('/api',  router)
 
 app.listen(3000, () =>
+
   console.log('Example app listening on port 3000!'),
 );
 
